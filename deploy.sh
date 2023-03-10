4cd45bb4dafc60386dceeefe5630a7ebc3a3e560
acidumirae <acidumirae@gmail.com>
2023-03-11 07:49:13 +0800

    Override version and increase verbosity in deploy script. Updated travis yaml to pass version

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@ ApiKey=$1
 Source=$2
 
 # List all packages with nuspecs to be packed and pushed
-nuget pack ./src/X12.Core.Hipaa/package.nuspec
-nuget pack ./src/X12.Core.Parsing/package.nuspec
-nuget pack ./src/X12.Core.Shared/package.nuspec
-nuget pack ./src/X12.Core.Specifications/package.nuspec
-nuget pack ./src/X12.Core.Sql/package.nuspec
-nuget pack ./src/X12.Core.Validation/package.nuspec
+nuget pack ./src/X12.Core.Hipaa/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Core.Parsing/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Core.Shared/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Core.Specifications/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Core.Sql/package.nuspec -Version $Version -Verbosity detailed
+nuget pack ./src/X12.Core.Validation/package.nuspec -Version $Version -Verbosity detailed
 
 nuget setApiKey $ApiKey -Source $Source -Verbosity detailed
 
