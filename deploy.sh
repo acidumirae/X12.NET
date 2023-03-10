536e630e54a435451099817a9012939477bf153e
acidumirae <acidumirae@gmail.com>
2023-03-11 07:51:26 +0800

    Switched to the TRAVIS_BUILD_NUMBER for build versioning. Updated travis.yaml to reflect this change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 ApiKey=$1
 Source=$2
+Version="5.0.${3}"
 
 # List all packages with nuspecs to be packed and pushed
 nuget pack ./src/X12.Core.Hipaa/package.nuspec -Version $Version -Verbosity detailed
